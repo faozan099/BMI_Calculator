e4380e05aa75eaa42ee798f1b9b0dd1077ab6906
Your Name <fauzanaunillah13@gmail.comgit config --global user.email fauzanaunillah13@gmail.com>
2023-09-26 20:21:59 +0700

    finish js

---
+++ script.js
@@ -0,0 +1,46 @@
+let button = document.getElementById("btn");
+
+button.addEventListener("click", () => {
+  const height = parseFloat(document.getElementById("height").value); // Menggunakan parseFloat untuk memungkinkan desimal
+  const weight = parseFloat(document.getElementById("weight").value); // Menggunakan parseFloat untuk memungkinkan desimal
+  const hasil = document.getElementById("output");
+  let height_status = false;
+  let weight_status = false;
+
+  if (isNaN(height) || height <= 0) {
+    document.getElementById("h-error").innerHTML =
+      "Please provide a valid height";
+  } else {
+    document.getElementById("h-error").innerHTML = "";
+    height_status = true;
+  }
+
+  if (isNaN(weight) || weight <= 0) {
+    // Menggunakan isNaN untuk memeriksa apakah bukan angka
+    document.getElementById("w-error").innerHTML =
+      "Please provide a valid weight";
+  } else {
+    document.getElementById("w-error").innerHTML = "";
+    weight_status = true;
+  }
+
+  if (height_status && weight_status) {
+    let heightCm = Math.pow(height / 100, 2);
+    let bmi = (weight / heightCm).toFixed(1);
+
+    if (bmi < 18.6) {
+      hasil.innerHTML = `Your BMI is <b>${bmi}</b>, which indicates <b>UNDER WEIGHT</b>`;
+    } else if (bmi >= 18.6 && bmi < 24.9) {
+      hasil.innerHTML = `Your BMI is <b>${bmi}</b>, which indicates <b>NORMAL</b>`;
+    } else if (bmi >= 25 && bmi < 29.9) {
+      hasil.innerHTML = `Your BMI is <b>${bmi}</b>, which indicates <b>OVERWEIGHT</b>`;
+    } else {
+      hasil.innerHTML = `Your BMI is <b>${bmi}</b>, which indicates <b>OBESITY</b>`;
+    }
+  } else {
+    hasil.innerHTML = "";
+  }
+});
+
+document.getElementById("w-error").style.color = "red";
+document.getElementById("h-error").style.color = "red";
